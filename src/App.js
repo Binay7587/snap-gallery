3c0b1d7d6b842370e8dde4a239a3a0b7356e7036
Binaya Karki <Binay7587@gmail.com>
2023-02-12 16:58:02 +0545

    Feat: Added Unsplash api to get a list of images statically

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { UNSPLASH_ACCESS_KEY } from './config/constants';
 import './App.css';
 
 const App = () => {
+    const [imageList, setImageList] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://api.unsplash.com/search/collections?client_id=${UNSPLASH_ACCESS_KEY}&per_page=12&query=Nepal`)
+            .then((res) => {
+                if (res.status === 200) {
+                    console.log(res.data.results)
+                    setImageList(res.data.results)
+                }
+            })
+    }, [])
+
     return (
         <div>
-            <h1>Snap Gallery</h1>
+            <h1 className="heading">Snap Gallery</h1>
+            <div className="image-list">
+                {imageList.map((image, index) => {
+                    return (
+                        <figure className="image">
+                            <img src={image.cover_photo.urls.regular} alt={image.cover_photo.alt_description} key={index} />
+                        </figure>
+                    )
+                })}
+            </div>
         </div>
     )
 }
