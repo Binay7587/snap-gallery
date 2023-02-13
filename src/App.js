47d35d0b86baea06194467c9e4a555c5923aa54b
Binaya Karki <Binay7587@gmail.com>
2023-02-13 18:41:07 +0545

    Feat: Added search functionality using unsplash Api

--- src/App.js
+++ src/App.js
@@ -4,26 +4,45 @@ import { UNSPLASH_ACCESS_KEY } from './config/constants';
 import './App.css';
 
 const App = () => {
+    const [search, setSearch] = useState("Nepal")
     const [imageList, setImageList] = useState([])
 
     useEffect(() => {
-        axios.get(`https://api.unsplash.com/search/collections?client_id=${UNSPLASH_ACCESS_KEY}&per_page=12&query=Nepal`)
-            .then((res) => {
-                if (res.status === 200) {
-                    console.log(res.data.results)
-                    setImageList(res.data.results)
-                }
-            })
+        fetchImages(search)
     }, [])
+
+    /**
+     * description: Fetch images from unsplash api
+     * @param {string} search - search query
+     */
+    const fetchImages = (query) => {
+        if (search) {
+            axios.get(`https://api.unsplash.com/search/collections?client_id=${UNSPLASH_ACCESS_KEY}&per_page=12&query=${query}`)
+                .then(res => {
+                    setImageList(res.data.results)
+                })
+                .catch(err => {
+                    console.log(err)
+                })
+        }
+    }
 
     return (
         <div>
             <h1 className="heading">Snap Gallery</h1>
+            <p className="sub-heading">Search for your favourite images</p>
+            <div className='search-bar'>
+                <input type="text" placeholder="Search Images"
+                    onChange={(e) => setSearch(e.target.value)}
+                    onKeyDown={(e) => e.key === "Enter" && fetchImages(search)} />
+                <button onClick={() => fetchImages(search)}>Search</button>
+            </div>
             <div className="image-list">
-                {imageList.map((image, index) => {
+                {imageList.length === 0 && <p className="no-image">No Images Found</p>}
+                {imageList.length > 0 && imageList.map((image, index) => {
                     return (
-                        <figure className="image">
-                            <img src={image.cover_photo.urls.regular} alt={image.cover_photo.alt_description} key={index} />
+                        <figure className="image" key={index}>
+                            <img src={image.cover_photo.urls.regular} alt={image.cover_photo.alt_description} />
                         </figure>
                     )
                 })}
